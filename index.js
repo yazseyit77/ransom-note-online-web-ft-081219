289b9c358f52cf56ce2c7f2316667e295228cc29
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-24 23:35:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function buildHistogram(magazine){
+  let histogram = {};
+
+  magazine.forEach(letter => {
+    if(histogram[letter]){
+      histogram[letter] +=1 ;
+    }else{
+      histogram[letter] = 1;
+    }
+  });
+  return histogram;
+}
+
+function canBuildNote(magazine, note){
+  let histogram = buildHistogram(magazine);
+
+  for(let i = 0; i < note.length; i++){
+    if(!histogram[note.charAt(i)]){
+      return false;
+    } else {
+      histogram[note.charAt(i)]  -= 1;
+    }
+  }
+  return true;
+}
