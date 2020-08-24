356f49b95ae80b9c9914ca95f5f2837c8e2e918c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-24 23:31:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
 function buildHistogram(magazine){
   let histogram = {};
+
+  magazine.forEach(letter => {
+    if(histogram[letter]){
+      histogram[letter];
+    }else{
+      histogram[letter] - 1;
+    }
+  });
+  return histogram;
 }
+
